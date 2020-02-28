52a82666545bb4c5864272393627e547373bb875
Rich Burdon <rich.burdon@gmail.com>
2020-02-28 01:04:13 -0500

    Updated response. Nodemon for development.

--- index.js
+++ index.js
@@ -4,31 +4,38 @@
 
 const debug = require('debug');
 const { createServer } = require('http');
+const micro = require('micro');
 const socketIO = require('socket.io');
-const micro = require('micro')
 
 const { SignalSwarmServer } = require('@geut/discovery-swarm-webrtc/server');
 
-let signal
-const server = createServer(micro(async (req, res) => {
-  const result = {}
-  signal.channels.forEach((peers, channel) => {
-    result[channel] = Array.from(peers.values())
-  })
-  return result
-}))
-
-signal = new SignalSwarmServer({ io: socketIO(server) });
+const { version } = require('./package.json');
 
 const port = process.env.PORT || 4000;
 
 const log = debug('signal');
 const error = debug('signal:error');
 
+let signal;
+
+const server = createServer(micro(async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  return {
+    channels: Array.from(signal.channels.keys()).map(channel => ({
+      channel,
+      peers: Array.from(signal.channels.get(channel).values())
+    })),
+    version
+  };
+}));
+
+signal = new SignalSwarmServer({ io: socketIO(server) });
+
 process.on('unhandledRejection', (err) => {
-  error('Unhandled rejection:', err.message);
+  error(`Unhandled: ${err.message}`);
 });
 
 server.listen(port, () => {
-  log('discovery-signal-webrtc running on %s', port);
+  log(`discovery-signal-webrtc running on ${port}`);
 });
